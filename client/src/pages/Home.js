37b8f39a554cf1fdacb64562c153931377453890
Gamy Aguirre <gamyag@gmail.com>
2022-12-12 18:22:58 -0700

    add fetch data

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react'
+
+
 const Home = () => {
+
+    const [products, setProducts] = useState(null)
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const response = await fetch('http://localhost:4000/api/products')
+            const json = await response.json()
+
+            if (response.ok) {
+                setProducts(json);
+            }
+        }
+
+        fetchProducts();
+
+    }, [])
+
     return (
         <div>
-            <h1>Home</h1>
+            <div className='Products'>
+                {products && products.map((product) => (
+                    <p key={product._id}>{product.name} - {product.SKU}</p>
+                ))}
+            </div>
         </div>
     )
 }
