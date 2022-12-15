0864449922a947733817bc78146c12e0227029bb
Gamy Aguirre <gamyag@gmail.com>
2022-12-15 06:35:56 -0700

    add key values to product table

--- client/src/components/ProductTable.js
+++ client/src/components/ProductTable.js
@@ -1,24 +1,28 @@
 const productTable = ({ products }) => {
     return (
         <div className="product-table">
-            <table>
-                <tr>
-                    <th width="200px">Name</th>
-                    <th width="200px">SKU</th>
-                    <th width="100px">Category</th>
-                    <th width="100px">Condition</th>
-                    <th>Quantity</th>
-                </tr>
-                {products && products.map((p) => (
-                    <tr>
-                        <td>{p.name}</td>
-                        <td>{p.SKU}</td>
-                        <td>{p.category}</td>
-                        <td>{p.condition}</td>
-                        <td>{p.quantity}</td>
-                    </tr>
-                ))}
-            </table>
+            <div>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th width="200px">Name</th>
+                            <th width="200px">SKU</th>
+                            <th width="100px">Category</th>
+                            <th width="100px">Condition</th>
+                            <th>Quantity</th>
+                        </tr>
+                        {products && products.map((p) => (
+                            <tr key={p._id}>
+                                <td>{p.name}</td>
+                                <td>{p.SKU}</td>
+                                <td>{p.category}</td>
+                                <td>{p.condition}</td>
+                                <td>{p.quantity}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
 
         fetchProducts();
 
-    }, [])
+    }, [dispatch])
 
     return (
         <div>
