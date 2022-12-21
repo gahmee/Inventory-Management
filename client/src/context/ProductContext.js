9f2e8684fb997b7dbfc7d7fc914e7600bb5ccc37
Gamy Aguirre <gamyag@gmail.com>
2022-12-20 18:27:27 -0700

    add sort to preserve product position when submitting an edit

--- client/src/context/ProductContext.js
+++ client/src/context/ProductContext.js
@@ -9,13 +9,15 @@ export const productsReducer = (state, action) => {
                 products: action.payload
             }
         case 'CREATE_PRODUCT':
+            const createSorted = [action.payload, ...state.products].sort((a, b) => a.SKU > b.SKU)
             return {
-                products: [action.payload, ...state.products]
+                products: [...createSorted]
             }
         case 'EDIT_PRODUCT':
             const filtered = state.products.filter((p) => p._id !== action.payload._id)
+            const editSorted = [action.payload, ...filtered].sort((a, b) => a.SKU > b.SKU)
             return {
-                products: [action.payload, ...filtered]
+                products: [...editSorted]
             }
         default:
             return state
