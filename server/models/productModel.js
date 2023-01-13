fc6f7bdc1dea267bc3d394bbf6066974f61a9a24
Gamy Aguirre <gamyag@gmail.com>
2023-01-13 06:40:21 -0700

    update product schema to begin new count page development

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -5,7 +5,11 @@ let productSchema = new mongoose.Schema({
     SKU: { type: String, required: true },
     category: { type: String, required: true },
     condition: { type: String, required: true },
+    previousQuantity: { type: Number, required: false },
+    received: { type: Number, required: false },
     quantity: { type: Number, required: true },
+    newQuantity: { type: Number, required: true },
+    gainLoss: { type: Number, required: false },
 })
 const Product = mongoose.model('Product', productSchema)
 module.exports = Product
