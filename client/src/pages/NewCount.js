7baaf81d4cb604e16d5470f4b1ff245ecd416353
Gamy Aguirre <gamyag@gmail.com>
2023-01-18 06:30:47 -0700

    add new count page groundwork

--- client/src/pages/NewCount.js
+++ client/src/pages/NewCount.js
@@ -1,8 +1,28 @@
-
+import { useEffect } from "react"
+import { useProductsContext } from '../hooks/useProductsContext'
+import NewCountForm from "../components/NewCountForm"
 
 const NewCount = () => {
+    const { products, dispatch } = useProductsContext()
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const response = await fetch('http://localhost:4000/api/products')
+            const json = await response.json()
+
+            if (response.ok) {
+                dispatch({ type: 'SET_PRODUCTS', payload: json });
+            }
+        }
+
+        fetchProducts();
+
+    }, [dispatch])
+
     return (
-        <div>NewCount</div>
+        <div>
+            <NewCountForm products={products} />
+        </div>
     )
 }
 
